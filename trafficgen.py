066fec3b86d338b4eeec723aa0f9db250ce1f7dc
Ike-Clinton <ikeclinton@gmail.com>
2017-02-01 09:52:39 -0500

    Simple PoC for sending payload with forged IP. Need to add 3 way handshake support before GET request is sent and ensure kernel does not RST

--- trafficgen.py
+++ trafficgen.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python
+# This program uses the scapy library to send HTTP Get
+# requests to a web server with forged src IPs
+# This has the intention of making it more difficult for blue teams
+# in cyber warfare games to identify the red teams IP
+
+from scapy.all import *
+src_ip = "192.168.20.20"
+dst_ip = "10.1.20.1"
+src_port = 6767
+dst_port = 80
+
+payload = "GET / HTTP 1.1"
+packet = IP(src=src_ip, dst=dst_ip) / TCP(sport=src_port, dport=dst_port) / payload
+send(packet)
